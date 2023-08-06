7af968c87cb12d9d54692a8d1111a5aec0913bcc
Hisbul Wathoni <hisbulwathoni663@gmail.com>
2023-08-06 11:28:21 +0800

    Update(Spwan): Update enemy spawn to 10 enemy will spawn in game

--- Assets/_Scripts/Classes/Game/GameLoopManager.cs
+++ Assets/_Scripts/Classes/Game/GameLoopManager.cs
@@ -16,16 +16,16 @@ public class GameLoopManager : MonoBehaviour
 
         StartCoroutine(GameLoop());
         InvokeRepeating("SummonTest", 0f, 1f);
-        InvokeRepeating("RemoveTest", 0f, 0.5f);
+        // InvokeRepeating("RemoveTest", 0f, 0.5f);
     }
 
-    void RemoveTest()
-    {
-        if(EntitySummoner.EnemiesInGame.Count > 0)
-        {
-            EntitySummoner.RemoveEnemy(EntitySummoner.EnemiesInGame[Random.Range(0, EntitySummoner.EnemiesInGame.Count)]);
-        }
-    }
+    // void RemoveTest()
+    // {
+    //     if(EntitySummoner.EnemiesInGame.Count > 0)
+    //     {
+    //         EntitySummoner.RemoveEnemy(EntitySummoner.EnemiesInGame[Random.Range(0, EntitySummoner.EnemiesInGame.Count)]);
+    //     }
+    // }
 
     void SummonTest()
     {
@@ -42,7 +42,11 @@ public class GameLoopManager : MonoBehaviour
             {
                 for(int i = 0; i < EnemyIDsToSummon.Count; i++)
                 {
-                    EntitySummoner.SummonEnemy(EnemyIDsToSummon.Dequeue());
+                    if(EntitySummoner.EnemiesInGame.Count < 10)
+                    {
+                        EntitySummoner.SummonEnemy(EnemyIDsToSummon.Dequeue());
+                    }
+                    
                 }
             }
 
