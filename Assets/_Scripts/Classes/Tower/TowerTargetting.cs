9af904d13eb3213963aaff84145d735a292e5686
Hisbul Wathoni <hisbulwathoni663@gmail.com>
2023-08-17 17:10:25 +0800

    Create(Script): Tower Targetting

---
+++ Assets/_Scripts/Classes/Tower/TowerTargetting.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerTargetting
+{   
+    // For handling target type/target position
+    public enum TargetType
+    {
+        First,
+        Last,
+        Close
+    }
+
+   public static Enemy GetTarget(TowerBehavior CurrentTower,TargetType TargetMethod)
+    {
+        return null;
+    }
+
+}
