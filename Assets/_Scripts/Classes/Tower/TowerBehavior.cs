0c4a6c9654bb1be6230a8c35b688acfea0e3ef39
Hisbul Wathoni <hisbulwathoni663@gmail.com>
2023-08-19 19:23:50 +0800

    Update TowerBehavior.cs

--- Assets/_Scripts/Classes/Tower/TowerBehavior.cs
+++ Assets/_Scripts/Classes/Tower/TowerBehavior.cs
@@ -26,7 +26,7 @@ public class TowerBehavior : MonoBehaviour
     {
         if(Target != null)
         {
-            TowerPivot.transform.rotation = Quaternion.LookRotation(Target.transform.position - transform.position);
+            TowerPivot.transform.rotation = Quaternion.LookRotation(Target.transform.position - transform.position, Vector3.forward);
         }
     }
 }
