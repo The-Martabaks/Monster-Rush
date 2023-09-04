527dbc8313ad1c9a3bcca1d596a1dc5c2483cf17
Hisbul Wathoni <hisbulwathoni663@gmail.com>
2023-09-04 11:17:30 +0800

    Feature(Money Display): Add money display

--- Assets/Tower/PlayerStats.cs
+++ Assets/Tower/PlayerStats.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI MoneyDisplayText;
     [SerializeField] private int StartingMoney;
     private int CurrentMoney;
     // Start is called before the first frame update
     private void Start()
     {
         CurrentMoney = StartingMoney;
+        MoneyDisplayText.SetText($"${StartingMoney}");
     }
 
     public void AddMoney(int MoneyToAdd)
     {
         CurrentMoney += MoneyToAdd;
+        MoneyDisplayText.SetText($"${StartingMoney}");
     }
 
     public int GetMoney()
