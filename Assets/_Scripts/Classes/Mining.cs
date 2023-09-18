67fd68338145bbf2659818a3a821021e898e028e
Hisbul Wathoni <hisbulwathoni663@gmail.com>
2023-09-18 23:21:25 +0800

    Update Mining.cs

--- Assets/_Scripts/Classes/Mining.cs
+++ Assets/_Scripts/Classes/Mining.cs
@@ -8,18 +8,18 @@ public class Mining : MonoBehaviour
     public GameObject Coinn;
     private GameObject _coin;
 
-    // Start is called before the first frame update
+    public Coroutine StartMining;
    
     void Update()
     {
         if(_coin == null && GameLoopManager.TotalCoin == 0)
         {
             _coin = Instantiate(Coinn,new Vector3(91.51f, -5.89f, 82.63454f), Quaternion.identity);
-            StartCoroutine(Coin());
+            StartMining = StartCoroutine(Coin());
         }
         else if(_coin != null && GameLoopManager.TotalCoin == 500)
         {
-            StopAllCoroutines();
+            StopCoroutine();
         }
         Debug.Log(GameLoopManager.TotalCoin);
     }
@@ -28,5 +28,9 @@ public class Mining : MonoBehaviour
         yield return new WaitForSeconds(5);
         GameLoopManager.TotalCoin += (100 * baseLevel);
         StartCoroutine(Coin());
+    }
+    public void StopCoroutine()
+    {
+        StopAllCoroutines();
     }
 }
