c29868c80bde56f24f1b7eb40fb16881b80aaf00
Hisbul Wathoni <hisbulwathoni663@gmail.com>
2023-09-27 16:02:06 +0800

    Update Coin.cs

--- Assets/_Scripts/Classes/Coin.cs
+++ Assets/_Scripts/Classes/Coin.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     private float speed = 300.0f;
-    // private PlayerStats PlayerStatistics;
-    // void Start()
-    // {
-    //     PlayerStatistics = FindAnyObjectByType<PlayerStats>();
-    // }
+    private PlayerStats PlayerStatistics;
+    void Start()
+    {
+        PlayerStatistics = FindAnyObjectByType<PlayerStats>();
+    }
     void Update()
     {
         transform.Rotate(Vector3.up * speed * Time.deltaTime); 
     }
-    // void OnTriggerEnter(Collider col)
-    // {
-    //     PlayerStatistics.AddMoney(GameLoopManager.TotalCoin);
-    //     GameLoopManager.TotalCoin = 0;
-    //     Destroy(gameObject);
-    // }
+    void OnTriggerEnter(Collider col)
+    {
+        PlayerStatistics.AddMoney(GameLoopManager.TotalCoin);
+        GameLoopManager.TotalCoin = 0;
+        Destroy(gameObject);
+    }
 }
